8079ccd0c3fb6046338c67839baae0de550ba283
Marveen Riskwait <radiostation87.7.7@gmail.com>
2026-03-16 10:36:36 +0000

    use state entendido, gracias profe!

--- src/js/components/Semaforo.jsx
+++ src/js/components/Semaforo.jsx
@@ -5,44 +5,17 @@ import React, { useState } from "react";
 
 const Semaforo = () => {
 
-    const [redLight, setRedLight] = useState("btn-secondary");
-    const [yellowLight, setYellowLigth] = useState("btn-secondary");
-    const [greenLigth, setGreenLigth] = useState("btn-secondary");
-
-
-
-    const changeToRedLight = () => {
-        if (redLight === "btn-secondary") {
-            setRedLight("btn-danger");
-        } else {
-            setRedLight("btn-secondary");
-        }
-    };
-
-
-    const changeToYellowLight = () => {
-        if (yellowLight === "btn-secondary") {
-            setYellowLigth("btn-warning");
-        } else {
-            setYellowLigth("btn-secondary");
-        }
-    };
-
-    const changeToGreenLight = () => {
-        if (greenLigth === "btn-secondary") {
-            setGreenLigth("btn-success");
-        } else {
-            setGreenLigth("btn-secondary");
-        }
-    };
+    const [color, setColor] = useState("null");
+    
+    
 
     return <>
     <div className="bg-dark mx-auto" style={{width:"3rem", height:"7rem"}}></div>
     <div className="d-flex justify-content-center">
         <div className="d-flex flex-column justify-content-around border border-5 rounded-5 border-secondary bg-dark d-block "> 
-            <div onMouseDown={changeToRedLight} onMouseUp={changeToRedLight} className={"btn border-black border-2 rounded-circle mx-3 my-4 " + redLight } style={{ width: "8rem", height: "8rem" }}></div>
-            <div onMouseDown={changeToYellowLight} onMouseUp={changeToYellowLight} className={"btn btn-secondary border-black border-2 rounded-circle mx-3 my-4 " + yellowLight} style={{ width: "8rem", height: "8rem" }}></div>
-            <div onMouseDown={changeToGreenLight} onMouseUp={changeToGreenLight} className={"btn btn-secondary border-black border-2 rounded-circle mx-3 my-4 " + greenLigth} style={{ width: "8rem", height: "8rem" }}></div>
+            <button onClick={()=>setColor("red")} className={`btn border-black border-2 rounded-circle mx-3 my-4 ${color === "red"? "btn-danger" : "btn-secondary"}`  } style={{ width: "8rem", height: "8rem" }}></button>
+            <button onClick={()=>setColor("yellow")} className={`btn btn-secondary border-black border-2 rounded-circle mx-3 my-4 ${color === "yellow"? "btn-warning" : "btn-secondary"}` } style={{ width: "8rem", height: "8rem" }}></button>
+            <button onClick={()=>setColor("green")} className={`btn btn-secondary border-black border-2 rounded-circle mx-3 my-4 ${color === "green"? "btn-success" : "btn-secondary"}`} style={{ width: "8rem", height: "8rem" }}></button>
         </div>
     </div>
     <div className="bg-dark mx-auto" style={{width:"3rem", height:"23rem"}}></div>
